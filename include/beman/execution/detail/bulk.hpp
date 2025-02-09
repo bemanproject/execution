9e789a3c5af410db9c99c67722f4f58066da56c7
Andres Salamanca <andrealebarbaritos@gmail.com>
2025-02-09 18:42:59 -0500

    Align Bulk Adaptor Sender completion signatures with base sender

--- include/beman/execution/detail/bulk.hpp
+++ include/beman/execution/detail/bulk.hpp
@@ -4,11 +4,9 @@
 #ifndef INCLUDED_BEMAN_EXECUTION_DETAIL_BULK
 #define INCLUDED_BEMAN_EXECUTION_DETAIL_BULK
 
-#include "beman/execution/detail/error_types_of_t.hpp"
-#include "beman/execution/detail/meta_combine.hpp"
-#include "beman/execution/detail/meta_unique.hpp"
-#include "beman/execution/detail/set_stopped.hpp"
-#include "beman/execution/detail/value_types_of_t.hpp"
+#include "beman/execution/detail/callable.hpp"
+#include "beman/execution/detail/get_completion_signatures.hpp"
+#include "beman/execution/detail/nothrow_callable.hpp"
 #include <beman/execution/detail/basic_sender.hpp>
 #include <beman/execution/detail/completion_signatures.hpp>
 #include <beman/execution/detail/completion_signatures_for.hpp>
@@ -80,37 +78,56 @@ struct impls_for<bulk_t> : ::beman::execution::detail::default_impls {
     };
 };
 
-template <class Shape, class f, class Sender, class Env>
+template <typename T>
+struct print_type;
+
+// Specialization to trigger a compile-time error and print the type
+template <typename T>
+struct print_type {
+    static_assert(sizeof(T) == 0, "Debugging type...");
+};
+
+/*template <typename, typename, typename, typename,typename>
+struct fixed_completions_helper;
+template <typename F, typename Shape,typename Sender,typename Env, typename... Args>
+struct fixed_completions_helper<F, Shape,Sender, Env, completion_signatures<Args...>> {
+    using completions = ::beman::execution::value_types_of_t<Sender,Env>;
+    static_assert(std::is_invocable_v<F,  completions>,"Error: The function is not callable with the given arguments.");
+    using type = std::conditional_t<::beman::execution::detail::nothrow_callable<F, Args ...>,
+                                    completion_signatures<Args...>,
+                                    completion_signatures<Args..., set_error_t(std::exception_ptr)>>;
+};
+
+template <typename F, typename Shape, typename Sender , typename Env, typename completions>
+using fixed_completions = typename fixed_completions_helper<F, Shape, Sender , Env, completions>::type;*/
+
+
+
+
+template <typename, typename, typename>
+struct fixed_completions_helper;
+
+template <typename F, typename Shape, typename... Args>
+struct fixed_completions_helper<F, Shape, completion_signatures<Args...>> {
+    //static_assert(std::is_invocable_v<F, Shape, Args...>,"Error: The function is not callable with the given arguments.");
+    using type = std::conditional_t<::beman::execution::detail::nothrow_callable<F, Args ...>,
+                                    completion_signatures<Args...>,
+                                    completion_signatures<Args..., set_error_t(std::exception_ptr)>>;
+};
+
+template <typename F, typename Shape, typename Completions>
+using fixed_completions = typename fixed_completions_helper<F, Shape, Completions>::type;
+
+template <class Shape, class F, class Sender, class Env>
 struct completion_signatures_for_impl<
     ::beman::execution::detail::
-        basic_sender<::beman::execution::detail::bulk_t, ::beman::execution::detail::product_type<Shape, f>, Sender>,
+        basic_sender<::beman::execution::detail::bulk_t, ::beman::execution::detail::product_type<Shape, F>, Sender>,
     Env> {
+    
 
-    // Creates a completion signature for set_value_t
-    template <class... Args>
-    using make_value_completions =
-        ::beman::execution::completion_signatures<::beman::execution::set_value_t(const std::decay_t<Args>&...)>;
-
-    // Creates a completion signature for set_error_t
-    template <class... Args>
-    using make_error_completions =
-        ::beman::execution::completion_signatures<::beman::execution::set_error_t(const std::decay_t<Args>&)...>;
-
-    // Retrieves the value completion signatures from the Sender using Env,
-    // then applies make_value_completions to format them and merges all signatures.
-    using value_completions = ::beman::execution::
-        value_types_of_t<Sender, Env, make_value_completions, ::beman::execution::detail::meta::combine>;
-
-    // Retrieves the error completion signatures from the Sender using Env,
-    // then applies make_error_completions to format them.
-    using error_completions = ::beman::execution::error_types_of_t<Sender, Env, make_error_completions>;
-
-    using fixed_completions =
-        ::beman::execution::completion_signatures<::beman::execution::set_stopped_t(),
-                                                  ::beman::execution::set_error_t(std::exception_ptr)>;
-
-    using type = ::beman::execution::detail::meta::unique<
-        ::beman::execution::detail::meta::combine<fixed_completions, value_completions, error_completions>>;
+    using completions = decltype(get_completion_signatures(std::declval<Sender>(), std::declval<Env>()));
+    using type        = fixed_completions<F, Shape, completions>;
+    //print_type<type> debug;
 };
 
 } // namespace beman::execution::detail
