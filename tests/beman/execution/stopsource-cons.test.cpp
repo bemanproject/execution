907dc862a53e385836b80d760958589116d9ea6b
Dietmar Kühl <dietmar.kuehl@me.com>
2025-06-01 05:06:03 +0100

    fix a formatting and an unused error

--- tests/beman/execution/stopsource-cons.test.cpp
+++ tests/beman/execution/stopsource-cons.test.cpp
@@ -8,13 +8,12 @@
 #include <cstddef>
 #include <cstring>
 
+// NOLINTNEXTLINE(hicpp-no-malloc)
+#if false && (not defined(__clang__) || not defined(__SANITIZE_THREAD__))
 namespace {
 bool fail{};
-
 }
 
-// NOLINTNEXTLINE(hicpp-no-malloc)
-#if  false && (not defined(__clang__) || not defined(__SANITIZE_THREAD__))
 auto operator new(::std::size_t size) -> void* { return fail ? throw ::std::bad_alloc() : ::std::malloc(size); }
 
 auto operator delete(void* ptr) noexcept -> void { ::std::free(ptr); }                // NOLINT(hicpp-no-malloc)
@@ -22,7 +21,7 @@ auto operator delete(void* ptr, ::std::size_t) noexcept -> void { ::std::free(pt
 #endif
 
 TEST(stopsource_cons) {
-#if  false && (not defined(__clang__) || not defined(__SANITIZE_THREAD__))
+#if false && (not defined(__clang__) || not defined(__SANITIZE_THREAD__))
     // Reference: [stopsource.cons] p1
     try {
         ::test_std::stop_source source;
