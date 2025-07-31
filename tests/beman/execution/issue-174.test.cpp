a1378689244da0402a93594e69b909663ca5e638
Dietmar Kühl <dietmar.kuehl@me.com>
2025-07-31 20:39:29 +0100

    clang-format

--- tests/beman/execution/issue-174.test.cpp
+++ tests/beman/execution/issue-174.test.cpp
@@ -8,24 +8,19 @@
 namespace ex = beman::execution;
 
 namespace {
-    struct thread_loop: ex::run_loop {
-        std::thread thread{[this] { this->run(); }};
-        ~thread_loop() {
-            this->finish();
-            this->thread.join();
-        }
-    };
-}
+struct thread_loop : ex::run_loop {
+    std::thread thread{[this] { this->run(); }};
+    ~thread_loop() {
+        this->finish();
+        this->thread.join();
+    }
+};
+} // namespace
 
 int main() {
     thread_loop ex_context1;
     thread_loop ex_context2;
 
-    ex::sync_wait(
-          ex::just()
-        | ex::then([] { })
-        | ex::continues_on(ex_context1.get_scheduler())
-        | ex::then([] { })
-        | ex::continues_on(ex_context2.get_scheduler()) | ex::then([] {})
-    );
+    ex::sync_wait(ex::just() | ex::then([] {}) | ex::continues_on(ex_context1.get_scheduler()) | ex::then([] {}) |
+                  ex::continues_on(ex_context2.get_scheduler()) | ex::then([] {}));
 }
