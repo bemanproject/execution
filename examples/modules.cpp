eee67980c2af84a22e5c5bc7f48af2907f1869aa
ClausKlein <claus.klein@arcormail.de>
2025-12-08 00:52:06 +0100

    Feat: Prepare CXX_MODULES tests

--- examples/modules.cpp
+++ examples/modules.cpp
@@ -11,17 +11,17 @@ import std;
 
 #endif
 
-#if __cpp_modules < 201907L
-#include <beman/execution/execution.hpp>
-#else
 import beman_execution;
-#endif
 
 namespace ex = beman::execution;
 
 int main() {
+#ifdef USE_THIS_CODE
     auto [result] = ex::sync_wait(ex::when_all(ex::just(std::string("hello, ")), ex::just(std::string("world"))) |
                                   ex::then([](const auto& s1, const auto& s2) { return s1 + s2; }))
                         .value_or(std::tuple(std::string("oops")));
     std::cout << "result='" << result << "'\n";
+#else
+    std::println("ex::version = {}", ex::version);
+#endif
 }
