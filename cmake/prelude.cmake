3985d0df5fa64070da67fe6245342091f32d5b13
ClausKlein <claus.klein@arcormail.de>
2026-02-06 17:28:13 +0100

    Last try for tody

--- cmake/prelude.cmake
+++ cmake/prelude.cmake
@@ -33,9 +33,10 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 4.2)
 endif()
 # gersemi: on
 
-if(NOT APPLE)
-    return()
-endif()
+# FIXME: we need this for linux too! CK
+# if(NOT APPLE)
+#     return()
+# endif()
 
 # FIXME: clang++ we still needs to export CXX=clang++
 if("$ENV{CXX}" STREQUAL "" AND CMAKE_CXX_COMPILER)
