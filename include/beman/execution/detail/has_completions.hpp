da093fe403200c90475e23f393b23b9e071d6054
Emin Can Özen <ecoezen@gmx.com>
2026-02-28 22:44:16 +0300

    Fix Clang C++20 modules AST serialization bug with lambdas
    Clang fails to deserialize the unevaluated generic lambda used in the
    `has_completions` concept when building as a C++20 module. Extracted the
    lambda into a namespace-scope `constexpr` variable template to bypass
    the AST serialization crash.
    
    Extracting it to a constexpr variable caused another set of bugs
    with MSVC. therefore, lambda completely replaced with SFINAE style
    'struct has_completions_impl'

--- include/beman/execution/detail/has_completions.hpp
+++ include/beman/execution/detail/has_completions.hpp
@@ -5,6 +5,12 @@
 #define INCLUDED_BEMAN_EXECUTION_DETAIL_HAS_COMPLETIONS
 
 #include <beman/execution/detail/common.hpp>
+
+#ifdef BEMAN_HAS_IMPORT_STD
+import std;
+#else
+#include <type_traits>
+#endif
 #ifdef BEMAN_HAS_MODULES
 import beman.execution.detail.completion_signatures;
 import beman.execution.detail.valid_completion_for;
@@ -16,11 +22,16 @@ import beman.execution.detail.valid_completion_for;
 // ----------------------------------------------------------------------------
 
 namespace beman::execution::detail {
+
 template <typename Receiver, typename Completions>
-concept has_completions = requires(Completions* completions) {
-    []<::beman::execution::detail::valid_completion_for<Receiver>... Signatures>(
-        ::beman::execution::completion_signatures<Signatures...>*) {}(completions);
-};
+struct has_completions_impl : std::false_type {};
+
+template <typename Receiver, ::beman::execution::detail::valid_completion_for<Receiver>... Signatures>
+struct has_completions_impl<Receiver, ::beman::execution::completion_signatures<Signatures...>> : std::true_type {};
+
+template <typename Receiver, typename Completions>
+concept has_completions = has_completions_impl<Receiver, Completions>::value;
+
 } // namespace beman::execution::detail
 
 // ----------------------------------------------------------------------------
