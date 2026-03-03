20858b96bdf99221c4c273893bbe9ab08a7436e6
Dietmar Kühl <dietmar.kuehl@me.com>
2026-03-04 00:45:42 +0100

    applied AI review feedback

--- tests/beman/execution/include/test/optional_sender.hpp
+++ tests/beman/execution/include/test/optional_sender.hpp
@@ -1,21 +1,17 @@
-// tests/beman/execution/include/optional_sender.hpp                  -*-C++-*-
+// tests/beman/execution/include/test/optional_sender.hpp             -*-C++-*-
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef INCLUDED_TESTS_BEMAN_EXECUTION_INCLUDE_OPTIONAL_SENDER
-#define INCLUDED_TESTS_BEMAN_EXECUTION_INCLUDE_OPTIONAL_SENDER
+#ifndef INCLUDED_TESTS_BEMAN_EXECUTION_INCLUDE_TEST_OPTIONAL_SENDER
+#define INCLUDED_TESTS_BEMAN_EXECUTION_INCLUDE_TEST_OPTIONAL_SENDER
 
 #include <test/execution.hpp>
 #include <optional>
 #include <utility>
 #ifdef BEMAN_HAS_MODULES
 import beman.execution;
-import beman.execution.detail;
 #else
-#include <beman/execution/detail/just.hpp>
 #include <beman/execution/detail/receiver.hpp>
 #include <beman/execution/detail/sender.hpp>
-#include <beman/execution/detail/stopped_as_error.hpp>
-#include <beman/execution/detail/sync_wait.hpp>
 #endif
 
 // ----------------------------------------------------------------------------
@@ -28,7 +24,7 @@ struct optional_sender {
 
     optional_sender() = default;
 
-    explicit optional_sender(T value) noexcept : opt(value) {}
+    explicit optional_sender(T value) noexcept : opt(std::move(value)) {}
 
     template <typename, typename...>
     static consteval auto get_completion_signatures() noexcept -> completion_signatures {
@@ -42,7 +38,7 @@ struct optional_sender {
             auto start() & noexcept -> void {
                 test::use_type<operation_state_concept>(); // make -Werror=unused-local-typedefs happy
                 if (opt_) {
-                    test_std::set_value(std::move(rcvr_), *opt_);
+                    test_std::set_value(std::move(rcvr_), std::move(*opt_));
                 } else {
                     test_std::set_stopped(std::move(rcvr_));
                 }
@@ -51,7 +47,7 @@ struct optional_sender {
             Rcvr             rcvr_;
             std::optional<T> opt_;
         };
-        return state{rcvr, std::move(opt)};
+        return state{std::move(rcvr), std::move(opt)};
     }
 
     std::optional<T> opt;
