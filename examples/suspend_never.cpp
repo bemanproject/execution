2adf88ed5ad5ddb89dfaf0f5286300ce236542bf
Dietmar Kühl <dietmar.kuehl@me.com>
2026-04-09 07:15:13 +0100

    another attempt at fixing suspend_never

--- examples/suspend_never.cpp
+++ examples/suspend_never.cpp
@@ -15,13 +15,23 @@ void* operator new(std::size_t n) {
     std::cout << "global new(" << n << ")->" << p << "\n";
     return p;
 }
-void operator delete(void* ptr) noexcept { std::cout << "global operator delete()" << ptr << "\n"; }
+void operator delete(void* ptr) noexcept {
+    std::cout << "global operator delete()" << ptr << "\n";
+    std::free(ptr);
+}
 
 int main() {
     struct resource : std::pmr::memory_resource {
-        void* do_allocate(std::size_t n, std::size_t) override { return std::malloc(n); }
-        void  do_deallocate(void* p, std::size_t n, std::size_t) override { std::free(p); }
-        bool  do_is_equal(const std::pmr::memory_resource& other) const noexcept override { return this == &other; }
+        void* do_allocate(std::size_t n, std::size_t a) override {
+            auto p{std::malloc(n)};
+            std::cout << "resource::allocate(" << n << ", " << a << ") -> " << p << "\n";
+            return p;
+        }
+        void do_deallocate(void* p, std::size_t n, std::size_t a) override {
+            std::cout << "resource::deallocate(" << p << ", " << n << ", " << a << ")\n";
+            std::free(p);
+        }
+        bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override { return this == &other; }
     } res{};
 
     ex::sync_wait(ex::write_env(
