c2c77625969c7e27cc8bfef5602fa06b7a97e3a0
JorgeV92 <jorgevmg31@gmail.com>
2026-05-20 22:57:39 -0500

    format fix

--- tests/beman/execution/exec-parallel-scheduler.test.cpp
+++ tests/beman/execution/exec-parallel-scheduler.test.cpp
@@ -10,7 +10,7 @@
 #include <type_traits>
 #include <utility>
 #include <test/execution.hpp>
-#ifdef BEMAN_HAS_MODULES 
+#ifdef BEMAN_HAS_MODULES
 import beman.execution;
 import beman.execution.detail.schedule_result_t;
 #else
@@ -53,16 +53,15 @@ auto test_parallel_scheduler_synopsis() -> void {
     static_assert(::std::same_as<test_std::schedule_result_t<test_std::parallel_scheduler>,
                                  test_std::parallel_scheduler::sender>);
     static_assert(test_std::sender<test_std::parallel_scheduler::sender>);
-    static_assert(::std::same_as<
-                  decltype(test_std::get_completion_signatures<test_std::parallel_scheduler::sender>()),
-                  test_std::completion_signatures<test_std::set_value_t(),
-                                                  test_std::set_error_t(::std::exception_ptr),
-                                                  test_std::set_stopped_t()>>);
+    static_assert(::std::same_as<decltype(test_std::get_completion_signatures<test_std::parallel_scheduler::sender>()),
+                                 test_std::completion_signatures<test_std::set_value_t(),
+                                                                 test_std::set_error_t(::std::exception_ptr),
+                                                                 test_std::set_stopped_t()>>);
 
-    static_assert(noexcept(test_std::get_forward_progress_guarantee(
-        ::std::declval<const test_std::parallel_scheduler&>())));
+    static_assert(
+        noexcept(test_std::get_forward_progress_guarantee(::std::declval<const test_std::parallel_scheduler&>())));
     static_assert(::std::same_as<decltype(test_std::get_forward_progress_guarantee(
-                                      ::std::declval<const test_std::parallel_scheduler&>())),
+                                     ::std::declval<const test_std::parallel_scheduler&>())),
                                  test_std::forward_progress_guarantee>);
 }
 
@@ -72,8 +71,7 @@ auto test_replaceability_synopsis() -> void {
     static_assert(::std::is_abstract_v<replaceability::parallel_scheduler_backend>);
     static_assert(::std::derived_from<bulk_proxy, replaceability::receiver_proxy>);
     static_assert(::std::derived_from<backend, replaceability::parallel_scheduler_backend>);
-    static_assert(::std::same_as<decltype(::std::declval<proxy&>().template try_query<int>(0)),
-                                 ::std::optional<int>>);
+    static_assert(::std::same_as<decltype(::std::declval<proxy&>().template try_query<int>(0)), ::std::optional<int>>);
     static_assert(::std::same_as<decltype(replaceability::query_parallel_scheduler_backend()),
                                  ::std::shared_ptr<replaceability::parallel_scheduler_backend>>);
 }
