56d07f146a0748ed7feec6b17e7ce3e80aecb82f
JorgeV92 <jorgevmg31@gmail.com>
2026-05-21 22:20:41 -0500

    update allocator_support P3980R1

--- tests/beman/execution/task-allocator_support.test.cpp
+++ tests/beman/execution/task-allocator_support.test.cpp
@@ -36,9 +36,55 @@ struct some_data {
     double data{};
 };
 
+struct allocation_counter {
+    std::size_t outstanding{};
+};
+
+template <typename T>
+struct tracking_allocator {
+    using value_type = T;
+
+    allocation_counter* counter{};
+
+    explicit tracking_allocator(allocation_counter& c) noexcept : counter(&c) {}
+
+    template <typename U>
+    tracking_allocator(const tracking_allocator<U>& other) noexcept : counter(other.counter) {}
+
+    auto allocate(std::size_t count) -> T* {
+        this->counter->outstanding += count * sizeof(T);
+        return static_cast<T*>(::operator new(count * sizeof(T), std::align_val_t(alignof(T))));
+    }
+
+    auto deallocate(T* ptr, std::size_t count) noexcept -> void {
+        ::operator delete(ptr, std::align_val_t(alignof(T)));
+        this->counter->outstanding -= count * sizeof(T);
+    }
+
+    template <typename U>
+    auto operator==(const tracking_allocator<U>& other) const noexcept -> bool {
+        return this->counter == other.counter;
+    }
+};
+
 template <typename Allocator>
 struct allocator_aware : some_data, test_detail::allocator_support<Allocator> {
     allocator_aware() : some_data() {}
+};
+
+template <typename T>
+concept supports_misplaced_allocator = requires(test_resource& resource) {
+    T::operator new(sizeof(T), 0, 1, std::allocator_arg, std::pmr::polymorphic_allocator<std::byte>{&resource});
+};
+
+template <typename T>
+concept supports_resource_pointer_allocator = requires(test_resource& resource) {
+    T::operator new(sizeof(T), std::allocator_arg, &resource);
+};
+
+template <typename Alloc>
+concept constructs_pmr_byte_allocator = requires(Alloc& alloc) {
+    std::pmr::polymorphic_allocator<std::byte>{alloc};
 };
 } // namespace
 
@@ -46,17 +92,45 @@ struct allocator_aware : some_data, test_detail::allocator_support<Allocator> {
 
 TEST(allocator_support) {
     using type = allocator_aware<std::pmr::polymorphic_allocator<std::byte>>;
+    static_assert(requires { type::operator new(sizeof(type)); });
+    static_assert(requires(test_resource& resource) {
+        type::operator new(sizeof(type), std::allocator_arg, std::pmr::polymorphic_allocator<std::byte>{&resource});
+    });
+    static_assert(requires(const some_data& object, test_resource& resource) {
+        type::operator new(
+            sizeof(type), object, std::allocator_arg, std::pmr::polymorphic_allocator<std::byte>{&resource});
+    });
+    static_assert(requires(allocation_counter& counter) {
+        type::operator new(sizeof(type), std::allocator_arg, tracking_allocator<int>{counter});
+    });
+    static_assert(not constructs_pmr_byte_allocator<tracking_allocator<int>>);
+    static_assert(not supports_misplaced_allocator<type>);
+    static_assert(not supports_resource_pointer_allocator<type>);
+
     [[maybe_unused]] std::unique_ptr<type> unused(new type{});
 
     test_resource resource{};
+    std::pmr::polymorphic_allocator<std::byte> allocator{&resource};
     ASSERT(resource.outstanding == 0u);
-    type* ptr{new (std::allocator_arg, &resource) type{}};
+    type* ptr{new (std::allocator_arg, allocator) type{}};
     ASSERT(resource.outstanding != 0u);
     ptr->~type();
     ASSERT(resource.outstanding != 0u);
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wmismatched-new-delete"
 #endif
-    type::operator delete(ptr, sizeof(type), std::allocator_arg, &resource);
+    type::operator delete(ptr, sizeof(type), std::allocator_arg, allocator);
     ASSERT(resource.outstanding == 0u);
+
+    some_data object{};
+    void*     raw{type::operator new(sizeof(type), object, std::allocator_arg, allocator)};
+    ASSERT(resource.outstanding != 0u);
+    type::operator delete(raw, sizeof(type), object, std::allocator_arg, allocator);
+    ASSERT(resource.outstanding == 0u);
+
+    allocation_counter counter{};
+    void* custom_raw{type::operator new(sizeof(type), std::allocator_arg, tracking_allocator<int>{counter})};
+    ASSERT(counter.outstanding != 0u);
+    type::operator delete(custom_raw, sizeof(type), std::allocator_arg, tracking_allocator<int>{counter});
+    ASSERT(counter.outstanding == 0u);
 }
